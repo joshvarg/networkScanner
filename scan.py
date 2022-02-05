cd7e9b8696aa93650eec009becb130183518d08e
joshvarg <78836623+joshvarg@users.noreply.github.com>
2022-02-05 12:36:27 -0500

    Write subnet2CIDR method for cleaner ARP construction

--- scan.py
+++ scan.py
@@ -1,6 +1,16 @@
+from tokenize import String
 import netifaces as nf
 import scapy.all as scapy
 import socket as s
+
+def subnet2CIDR(subnet, cidr):
+  classSplit = subnet.split('.')
+  for i in classSplit:
+    class2Binary = format(int(i), 'b')
+    cidr += class2Binary.count('1')
+
+  cidr = str(cidr)
+  return cidr
 
 hostaddr = s.gethostbyname(s.gethostname())
 if(hostaddr == '127.0.0.1'):
@@ -12,3 +22,10 @@ if(hostaddr == '127.0.0.1'):
 wifiaddrs = nf.ifaddresses('en0')
 netmask = wifiaddrs[nf.AF_INET][0]['netmask']
 print(netmask, hostaddr)
+cidr = 0
+
+hostaddr = hostaddr + '/' + subnet2CIDR(netmask, cidr)
+print(hostaddr)
+
+arp = scapy.ARP(pdst=hostaddr)
+arp.show()
